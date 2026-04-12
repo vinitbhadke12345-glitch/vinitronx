f02a93e993ef38cd1f6ab74a3eb5554b87d537e9
vinitbhadke12345-glitch <vinitbhadke12345@gmail.com>
2026-04-12 14:57:14 +0530

    Create App component with basic layout

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "black",
+      color: "white"
+    }}>
+      <h1>🚀 Vinitronx Live Successfully!</h1>
+    </div>
+  );
+}
